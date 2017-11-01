1f9e05681db37c622eb0fba3ce032644eaead031
mktcode <kontakt@markus-kottlaender.de>
2017-11-01 18:54:32 +0100

    nav animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 $(function () {
+  // waves
   let waveWhite = new TimelineMax();
   waveWhite.to($('#wave-white'), 20, {backgroundPositionX:-300, repeatDelay:0, repeat:-1, yoyo:true, ease: Power1.easeInOut});
   waveWhite.play();
@@ -10,4 +11,15 @@ $(function () {
   let waveGrey2 = new TimelineMax();
   waveGrey2.to($('#wave-grey2'), 40, {backgroundPositionX:-250, repeatDelay:0, repeat:-1, yoyo:true, ease: Power1.easeInOut});
   waveGrey2.play();
+
+  // nav
+  $(window).on('scroll', function () {
+    let nav = $('#navbar');
+    if ($(this).scrollTop() > 150) {
+      nav.addClass('scrolled');
+    } else {
+      nav.removeClass('scrolled');
+    }
+  });
+  $(window).scroll();
 });
